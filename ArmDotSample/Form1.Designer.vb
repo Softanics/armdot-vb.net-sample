e0708ab61e238aa4551ca6dfb39bd7a7fa9865b5
Artem Razin <razin@softanics.com>
2021-03-23 13:10:22 +0300

    Added project files

---
+++ ArmDotSample/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.ButtonCheckPassword = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(13, 13)
+        Me.TextBoxPassword.Multiline = True
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(397, 148)
+        Me.TextBoxPassword.TabIndex = 0
+        '
+        'ButtonCheckPassword
+        '
+        Me.ButtonCheckPassword.Location = New System.Drawing.Point(159, 167)
+        Me.ButtonCheckPassword.Name = "ButtonCheckPassword"
+        Me.ButtonCheckPassword.Size = New System.Drawing.Size(127, 23)
+        Me.ButtonCheckPassword.TabIndex = 1
+        Me.ButtonCheckPassword.Text = "Check password"
+        Me.ButtonCheckPassword.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 201)
+        Me.Controls.Add(Me.ButtonCheckPassword)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents ButtonCheckPassword As Button
+End Class
---
+++ ArmDotSample/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text
+Imports System.Security.Cryptography
+
+<Assembly: ArmDot.Client.ObfuscateNames>
+<Assembly: ArmDot.Client.HideStrings>
+<Assembly: ArmDot.Client.ObfuscateControlFlow>
+<Assembly: ArmDot.Client.ProtectEmbeddedResources>
+
+Public Class Form1
+    <ArmDot.Client.VirtualizeCode>
+    Private Sub ButtonCheckPassword_Click(sender As Object, e As EventArgs) Handles ButtonCheckPassword.Click
+        Dim hash As Byte() = (New SHA256Managed()).ComputeHash(Encoding.Unicode.GetBytes(TextBoxPassword.Text))
+
+        If hash(0) = 81 And hash(1) = 96 And hash(2) = 9 And hash(3) = 150 And
+        hash(4) = 45 And hash(5) = 146 And hash(6) = 51 And hash(7) = 201 And
+        hash(8) = 238 And hash(9) = 22 And hash(10) = 103 And hash(11) = 233 And
+        hash(12) = 209 And hash(13) = 135 And hash(14) = 107 And hash(15) = 39 And
+        hash(16) = 228 And hash(17) = 171 And hash(18) = 22 And hash(19) = 78 And
+        hash(20) = 218 And hash(21) = 213 And hash(22) = 11 And hash(23) = 131 And
+        hash(24) = 71 And hash(25) = 17 And hash(26) = 241 And hash(27) = 180 And
+        hash(28) = 118 And hash(29) = 9 And hash(30) = 163 And hash(31) = 249 Then
+            MessageBox.Show("The password is correct")
+        Else
+            MessageBox.Show("The password is wrong")
+        End If
+    End Sub
+End Class
